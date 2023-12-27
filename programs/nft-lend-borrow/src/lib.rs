a433049a4a65a2d774f06ef0bb8f538bf9341cae
jorgek00 <154838348+jorgek00@users.noreply.github.com>
2023-12-27 22:15:37 +0000

    build

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("HGzbsC4UwTU98CfBBRCBSUo2BvethGP9SbsTW52rSsVh");
 
 #[program]
 pub mod nft_lend_borrow {
